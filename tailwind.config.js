76adb544a4ce2bf906921a855776dfaef5a513b1
Ally Abdullah Zafar <49100580+dryad13@users.noreply.github.com>
2025-06-30 04:27:44 +0500

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      // Add this fontFamily block
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'], // This is your default font
+        display: ['Ethnocentric', 'sans-serif'], // This creates the 'font-display' utility
+      },
+    },
   },
   plugins: [],
 };
